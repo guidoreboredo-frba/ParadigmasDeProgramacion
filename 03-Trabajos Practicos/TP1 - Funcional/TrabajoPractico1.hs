7d3f25de61732f140e6fc5a846ceb60a499f1c77
Guido Reboredo <82243287+guidoreboredo-frba@users.noreply.github.com>
2021-05-05 19:47:22 -0300

    Add files via upload

---
+++ 03-Trabajos Practicos/TP1 - Funcional/TrabajoPractico1.hs
@@ -0,0 +1,19 @@
+{- 
+Module      :  TrabajoPractico1.hs
+Description :  Trabajo Práctico Numero 1 - Funcional
+Date        :  14.05.2021
+Grupo       :  Grupo 6 
+Name        :  Guido Reboredo, 
+Subject     :  PdeP K2054 - UTN FRBA
+
+Fuente de los deseos
+
+-}
+
+{- DEFINICION DE DATOS -}
+
+
+data Persona = Persona {edad :: Int, sueños :: Sueño, nombe :: String, felicidonios :: Int, habilidad :: [String]}
+
+
+{- PUNTO #1 -}
